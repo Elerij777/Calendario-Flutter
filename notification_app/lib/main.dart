8ad834fb0c8721e205c8f64c1b0168ae847ace33
Elerij777 <146883437+Elerij777@users.noreply.github.com>
2024-04-12 15:59:05 -0600

    Implementacion de interfaz de calendario, y un poco de diseño

--- notification_app/lib/main.dart
+++ notification_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:notification_app/ui/home_page.dart';
@@ -6,7 +7,22 @@ import 'package:notification_app/ui/services/theme_services.dart';
 import 'package:notification_app/ui/theme.dart';
 
 Future<void> main() async {
-WidgetsFlutterBinding.ensureInitialized();
+  // Configuración regional predeterminada
+  /*
+  final languageCode = ui.window.locale.languageCode;
+  final countryCode = ui.window.locale.countryCode;
+
+  String defaultLocale = languageCode ?? '';
+  if (countryCode != null) {
+    defaultLocale += '_$countryCode';
+  }
+
+  Intl.defaultLocale = defaultLocale;
+  */
+
+  // Inicialización de GetStorage
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   runApp(MyApp());
 }
@@ -15,14 +31,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: const Locale('es'),
+      supportedLocales: const [
+        Locale('es'),
+      ],
       theme: Themes.light,
       darkTheme: Themes.dark,
       //themeMode: ThemeMode.light,
       themeMode: ThemeService().theme,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      
-      home:  const HomePage(),
+
+      home: const HomePage(),
     );
   }
 }
---
+++ notification_app/lib/ui/widgets/custome_buttom.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:notification_app/ui/theme.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Function()? onTap;
+
+  const CustomButton({Key? key, required this.text, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 150,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: primaryClr,
+        ),
+        child: Center(
+          // Centra el texto horizontalmente y verticalmente dentro del contenedor
+          child: Text(
+            text,
+            textAlign: TextAlign.center, // Centra el texto horizontalmente
+            style: const TextStyle(
+              fontFamily: 'Lato',
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
