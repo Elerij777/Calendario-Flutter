1be9ac2b0774e1c22a8524eb28368e2c711d8813
Elerij777 <146883437+Elerij777@users.noreply.github.com>
2024-04-26 09:14:17 -0600

    Implementacion de graficas en el apartado de estadistica

--- notification_app/lib/models/task.dart
+++ notification_app/lib/models/task.dart
@@ -4,55 +4,56 @@ class Task {
   String? note;
   String? tipo;
   int? isCompleted;
-  String? date;
-  String? starTime;
-  String? endTime;
+  String date; // Cambiado a String no nulo
+  String starTime; // Cambiado a String no nulo
+  String endTime; // Cambiado a String no nulo
   int? color;
   int? reminder;
   String? repeat;
-  Task(
-      {this.id,
-      this.title,
-      this.note,
-      this.tipo,
-      this.isCompleted,
-      this.date,
-      this.starTime,
-      this.endTime,
-      this.color,
-      this.reminder,
-      this.repeat,
-      });
 
-  Task.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    note = json['note'];
-    tipo = json['tipo'];
-    isCompleted = json['isCompleted'];
-    date  = json['date'];
-    starTime = json['startTime'];
-    endTime = json['endTime'];
-    color = json['color'];
-    reminder = json['remind'];
-    repeat = json['repeat'];
-  }
+  Task({
+    this.id,
+    this.title,
+    this.note,
+    this.tipo,
+    this.isCompleted,
+    required this.date,
+    required this.starTime,
+    required this.endTime,
+    this.color,
+    this.reminder,
+    this.repeat,
+  });
 
-  Map<String, dynamic> toJson(){
-    final Map <String,dynamic> data =new Map <String, dynamic>();
-    data['id']=this.id;
-    data['title']=this.title;
-    data['note']=this.note; 
-    data['tipo']=this.tipo;
-    data['isCompleted']=this.isCompleted;  
-    data['date']=this.date;
-    data['startTime']=this.starTime;
-    data['endTime']=this.endTime;
-    data ['color']=this.color;
-    data ['remind']=this.reminder;
-    data ['repeat']=this.repeat;
+  Task.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        note = json['note'],
+        tipo = json['tipo'],
+        isCompleted = json['isCompleted'],
+        date = json['date'] ?? "", // Manejar el caso en que date sea nulo
+        starTime = json['startTime'] ??
+            "", // Manejar el caso en que startTime sea nulo
+        endTime =
+            json['endTime'] ?? "", // Manejar el caso en que endTime sea nulo
+        color = json['color'],
+        reminder = json['remind'],
+        repeat = json['repeat'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['note'] = this.note;
+    data['tipo'] = this.tipo;
+    data['isCompleted'] = this.isCompleted;
+    data['date'] = this.date;
+    data['startTime'] = this.starTime;
+    data['endTime'] = this.endTime;
+    data['color'] = this.color;
+    data['remind'] = this.reminder;
+    data['repeat'] = this.repeat;
 
     return data;
-
   }
 }
--- notification_app/lib/ui/widgets/Estadistics.dart
+++ notification_app/lib/ui/widgets/Estadistics.dart
@@ -1,3 +1,4 @@
+import 'dart:core';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -12,6 +13,19 @@ class EstadisticaScreen extends StatefulWidget {
 class _EstadisticaScreenState extends State<EstadisticaScreen> {
   int _completedTasks = 0;
   int _incompleteTasks = 0;
+  int _estudiosTasks = 0;
+  int _trabajoTasks = 0;
+  int _saludTasks = 0;
+  int _personalTasks = 0;
+  double _semana1Minutos = 0.0;
+  double _semana2Minutos = 0.0;
+  double _semana3Minutos = 0.0;
+  double _semana4Minutos = 0.0;
+  double _semana5Minutos = 0.0;
+  late double tiempototal5semanas = 0.0;
+
+  List<String> _weekLabels = [];
+  List<int> _timeSpentData = [];
 
   @override
   void initState() {
@@ -20,12 +34,113 @@ class _EstadisticaScreenState extends State<EstadisticaScreen> {
   }
 
   void _loadTasks() async {
+    _loadCompletedTaskStats();
+    await _loadTimeSpentLastFiveWeeks();
+  }
+
+  Future<void> _loadTimeSpentLastFiveWeeks() async {
+    DateTime currentDate = DateTime.now();
+    DateTime fiveWeeksAgo = currentDate.subtract(Duration(days: 5 * 7));
+
+    List<Task> tasks = await DBHelper.queryTasks();
+    double semana1Minutos = 0.0;
+    double semana2Minutos = 0.0;
+    double semana3Minutos = 0.0;
+    double semana4Minutos = 0.0;
+    double semana5Minutos = 0.0;
+
+    tasks.forEach((task) {
+      String formattedDate =
+          '${task.date.split('/')[2]}-${task.date.split('/')[0].padLeft(2, '0')}-${task.date.split('/')[1].padLeft(2, '0')}';
+      DateTime endTime = DateTime.parse("$formattedDate ${task.endTime}");
+
+      if (task.isCompleted == 1 && endTime.isAfter(fiveWeeksAgo)) {
+        DateTime taskWeek = _getStartOfWeek(endTime);
+        int taskDuration = _calculateTaskDuration(task);
+
+        if (taskWeek.isAtSameMomentAs(currentDate)) {
+          semana1Minutos += taskDuration;
+        } else if (taskWeek.add(Duration(days: 7)).isAfter(currentDate)) {
+          semana2Minutos += taskDuration;
+        } else if (taskWeek.add(Duration(days: 14)).isAfter(currentDate)) {
+          semana3Minutos += taskDuration;
+        } else if (taskWeek.add(Duration(days: 21)).isAfter(currentDate)) {
+          semana4Minutos += taskDuration;
+        } else {
+          semana5Minutos += taskDuration;
+        }
+      }
+    });
+
+    setState(() {
+      _semana1Minutos = semana1Minutos;
+      _semana2Minutos = semana2Minutos;
+      _semana3Minutos = semana3Minutos;
+      _semana4Minutos = semana4Minutos;
+      _semana5Minutos = semana5Minutos;
+      tiempototal5semanas = semana1Minutos +
+          semana2Minutos +
+          semana3Minutos +
+          semana4Minutos +
+          semana5Minutos;
+    });
+  }
+
+  DateTime _getStartOfWeek(DateTime date) {
+    return date.subtract(Duration(days: date.weekday - 1));
+  }
+
+  int _calculateTaskDuration(Task task) {
+    DateTime startTime = DateTime(
+      int.parse(task.date.split('/')[2]), // Año
+      int.parse(task.date.split('/')[0]), // Mes
+      int.parse(task.date.split('/')[1]), // Día
+      int.parse(task.starTime.split(':')[0]), // Hora
+      int.parse(task.starTime.split(':')[1]), // Minuto
+    );
+
+    DateTime endTime = DateTime(
+      int.parse(task.date.split('/')[2]), // Año
+      int.parse(task.date.split('/')[0]), // Mes
+      int.parse(task.date.split('/')[1]), // Día
+      int.parse(task.endTime.split(':')[0]), // Hora
+      int.parse(task.endTime.split(':')[1]), // Minuto
+    );
+
+    return endTime.difference(startTime).inMinutes;
+  }
+
+  DateTime _getEndOfWeek(DateTime date) {
+    int currentWeekday = date.weekday;
+
+    int difference = 7 - currentWeekday;
+    return date.add(Duration(days: difference));
+  }
+
+  void _loadCompletedTaskStats() async {
     List<Task> tasks = await DBHelper.queryTasks();
     int completedCount = tasks.where((task) => task.isCompleted == 1).length;
     int incompleteCount = tasks.length - completedCount;
+    int estudiosCount = tasks
+        .where((task) => task.tipo == 'Estudios' && task.isCompleted == 1)
+        .length;
+    int trabajoCount = tasks
+        .where((task) => task.tipo == 'Trabajo' && task.isCompleted == 1)
+        .length;
+    int saludCount = tasks
+        .where((task) => task.tipo == 'Salud' && task.isCompleted == 1)
+        .length;
+    int personalCount = tasks
+        .where((task) => task.tipo == 'Personal' && task.isCompleted == 1)
+        .length;
+
     setState(() {
       _completedTasks = completedCount;
       _incompleteTasks = incompleteCount;
+      _estudiosTasks = estudiosCount;
+      _trabajoTasks = trabajoCount;
+      _saludTasks = saludCount;
+      _personalTasks = personalCount;
     });
   }
 
@@ -35,49 +150,210 @@ class _EstadisticaScreenState extends State<EstadisticaScreen> {
       appBar: AppBar(
         title: Text('Estadísticas'),
       ),
-      body: Center(
-        child: _completedTasks == 0 && _incompleteTasks == 0
-            ? CircularProgressIndicator()
-            : Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Card(
-                  elevation: 4,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        title: Text('Estadísticas de tareas completas'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: SizedBox(
-                          width: 200,
-                          height: 210,
-                          child: CustomPaint(
-                            size: Size(200, 200),
-                            painter: PieChartPainter(
-                              completePercent: _completedTasks /
-                                  (_completedTasks + _incompleteTasks),
-                              completeColor: Colors.green,
-                              incompleteColor: Colors.red,
+      body: SingleChildScrollView(
+        child: Center(
+          child: _completedTasks == 0 && _incompleteTasks == 0
+              ? CircularProgressIndicator()
+              : Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(children: [
+                    Card(
+                      elevation: 4,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            title: Text('Estadísticas de tareas completas'),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: SizedBox(
+                              width: 200,
+                              height: 210,
+                              child: CustomPaint(
+                                size: Size(200, 200),
+                                painter: PieChartPainter(
+                                  percentages: [
+                                    _completedTasks /
+                                        (_completedTasks + _incompleteTasks),
+                                    _incompleteTasks /
+                                        (_completedTasks + _incompleteTasks),
+                                  ],
+                                  colors: [Colors.green, Colors.red],
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          _buildLegend('Incompletas', Colors.red),
-                          _buildLegend('Completadas', Colors.green),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              _buildLegend(
+                                  "Incompletas ${(_incompleteTasks / (_completedTasks + _incompleteTasks) * 100).toStringAsFixed(1)}%",
+                                  Colors.red),
+                              _buildLegend(
+                                  'Completadas ${(_completedTasks / (_completedTasks + _incompleteTasks) * 100).toStringAsFixed(1)}%',
+                                  Colors.green),
+                            ],
+                          ),
+                          Container(
+                            height: 10,
+                          )
                         ],
                       ),
-                      Container(
-                        height: 10,
-                      )
-                    ],
-                  ),
+                    ),
+
+                    SizedBox(height: 20), // Espacio entre las dos tarjetas
+                    Card(
+                      elevation: 4,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            title: Text('Tareas completadas por tipo'),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: SizedBox(
+                              width: 200,
+                              height: 210,
+                              child: CustomPaint(
+                                size: Size(200, 200),
+                                painter: PieChartPainter(
+                                  percentages: [
+                                    _estudiosTasks / _completedTasks,
+                                    _trabajoTasks / _completedTasks,
+                                    _saludTasks / _completedTasks,
+                                    _personalTasks / _completedTasks,
+                                  ],
+                                  colors: [
+                                    Colors.blue,
+                                    Colors.orange,
+                                    Colors.red,
+                                    Colors.purple,
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Column(children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildLegend(
+                                    'Estudios ${(_estudiosTasks / _completedTasks * 100).toStringAsFixed(1)}%',
+                                    Colors.blue),
+                                _buildLegend(
+                                    'Trabajo ${(_trabajoTasks / _completedTasks * 100).toStringAsFixed(1)}%',
+                                    Colors.orange),
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildLegend(
+                                    'Salud ${(_saludTasks / _completedTasks * 100).toStringAsFixed(1)}%',
+                                    Colors.red),
+                                _buildLegend(
+                                    'Personal ${(_personalTasks / _completedTasks * 100).toStringAsFixed(1)}%',
+                                    Colors.purple),
+                              ],
+                            )
+                          ]),
+                          Container(
+                            height: 10,
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Card(
+                      elevation: 4,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            title: Text('Ultimas 5 semanas'),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: SizedBox(
+                              width: 200,
+                              height: 210,
+                              child: CustomPaint(
+                                size: Size(200, 200),
+                                painter: PieChartPainter(
+                                  percentages: [
+                                    _semana1Minutos / tiempototal5semanas,
+                                    _semana2Minutos / tiempototal5semanas,
+                                    _semana3Minutos / tiempototal5semanas,
+                                    _semana4Minutos / tiempototal5semanas,
+                                    _semana5Minutos / tiempototal5semanas,
+                                  ],
+                                  colors: [
+                                    Colors.blue,
+                                    Colors.orange,
+                                    Colors.red,
+                                    Colors.purple,
+                                    Colors.green,
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Column(children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildLegend(
+                                  'Semana 1: ${_semana1Minutos.toStringAsFixed(2)} minutos',
+                                  Colors.blue,
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [
+                                _buildLegend(
+                                    'Semana 2: ${_semana2Minutos.toStringAsFixed(2)} minutos',
+                                    Colors.orange),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildLegend(
+                                    'Semana 3: ${_semana3Minutos.toStringAsFixed(2)} minutos',
+                                    Colors.red),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildLegend(
+                                    'Semana 4: ${_semana4Minutos.toStringAsFixed(2)} minutos',
+                                    Colors.purple),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildLegend(
+                                    'Semana 5: ${_semana5Minutos.toStringAsFixed(2)} minutos',
+                                    Colors.green),
+                              ],
+                            )
+                          ]),
+                          SizedBox(height: 10),
+                          Container(
+                            height: 10,
+                          )
+                        ],
+                      ),
+                    ),
+                  ]),
                 ),
-              ),
+        ),
       ),
     );
   }
@@ -98,14 +374,12 @@ class _EstadisticaScreenState extends State<EstadisticaScreen> {
 }
 
 class PieChartPainter extends CustomPainter {
-  final double completePercent;
-  final Color completeColor;
-  final Color incompleteColor;
+  final List<double> percentages;
+  final List<Color> colors;
 
   PieChartPainter({
-    required this.completePercent,
-    required this.completeColor,
-    required this.incompleteColor,
+    required this.percentages,
+    required this.colors,
   });
 
   @override
@@ -115,25 +389,21 @@ class PieChartPainter extends CustomPainter {
 
     final paint = Paint()..style = PaintingStyle.fill;
 
-    // Dibujar la parte completa del pastel
-    paint.color = completeColor;
-    canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius),
-      -pi / 2,
-      2 * pi * completePercent,
-      true,
-      paint,
-    );
+    double startAngle = -pi / 2;
+    double endAngle = 0.0;
 
-    // Dibujar la parte incompleta del pastel
-    paint.color = incompleteColor;
-    canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius),
-      2 * pi * completePercent - pi / 2,
-      2 * pi * (1 - completePercent),
-      true,
-      paint,
-    );
+    for (int i = 0; i < percentages.length; i++) {
+      paint.color = colors[i];
+      endAngle = startAngle + 2 * pi * percentages[i];
+      canvas.drawArc(
+        Rect.fromCircle(center: center, radius: radius),
+        startAngle,
+        endAngle - startAngle,
+        true,
+        paint,
+      );
+      startAngle = endAngle;
+    }
   }
 
   @override
@@ -143,11 +413,12 @@ class PieChartPainter extends CustomPainter {
 
 
 
+
 /*
 import 'package:flutter/material.dart';
 import 'package:notification_app/db/db_helper.dart';
 import 'package:notification_app/models/task.dart';
-import 'package:community_charts_flutter/pie_chart.dart';
+
 
 class EstadisticaScreen extends StatefulWidget {
   @override
@@ -202,8 +473,4 @@ class _EstadisticaScreenState extends State<EstadisticaScreen> {
       ),
     );
   }
-}
-
-
-
-*/
+}*/
